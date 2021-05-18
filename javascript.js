e704b49a76ba0cd9a747b241ab081b009e63f207
Joohi Jain <juhijain1001@gmail.com>
2021-05-19 01:35:11 +0530

    added webiste files

---
+++ javascript.js
@@ -0,0 +1,45 @@
+var socialtags = [{
+    img: "img/github.svg",
+    link: "https://github.com/JoohiJ"
+}, {
+    img: "img/linkedin.svg",
+    link: "https://www.linkedin.com/in/joohi-jain-5115ab153/"
+}, {
+    img: "img/twitter.svg",
+    link: "https://twitter.com/juhijain93"
+}]
+
+function loadSocialIcons() {
+    var socialtext = ""
+    for (x in socialtags) {
+        var data = `<a class="center" href="` + socialtags[x].link + `"> <img class="center" style="padding: 5px!important;width: 40px;" src="` + socialtags[x].img + `"></a>`
+        socialtext += data
+    }
+    document.getElementById("social-list").innerHTML = socialtext;
+}
+
+loadSocialIcons()
+
+function dayNightToggle() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var check = window.localStorage.getItem('light-mode');
+    if (check == "light") {
+        window.localStorage.clear();
+    } else {
+        window.localStorage.setItem('light-mode', 'light');
+    }
+
+}
+
+function checkDayNight() {
+    var check = window.localStorage.getItem('light-mode');
+    var element = document.body;
+    if (check == "light") {
+        element.classList.add("light-mode");
+    } else {
+        element.classList.remove("light-mode");
+    }
+}
+
+checkDayNight()
